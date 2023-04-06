5cd722ad374a58562249c4fb4cce4eaca67d8c21
OmniGirl <okang.etta@gmail.com>
2023-04-06 12:25:48 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,22 +7,16 @@
  * @h: pointer to the head of the linked list
  * Return: the number of nodes in the linked list
  */
+
 size_t print_listint(const listint_t *h)
 {
-size_t node_count = 0;
+size_t num_nodes = 0;
 
 while (h != NULL)
 {
-if (h->n == NULL)
-{
-return (1);
-}
-else
-{
 printf("%d\n", h->n);
-}
-node_count++;
+num_nodes++;
 h = h->next;
 }
-return (node_count);
+return (num_nodes);
 }
