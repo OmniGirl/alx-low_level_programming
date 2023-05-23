b0f177fb2f53b2e35d50e3e60562d1ada5099336
OmniGirl <okang.etta@gmail.com>
2023-05-22 19:08:12 -0700

    update

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * Description - call C functions from Python
+ */
+ 
+int add(int a, int b)
+{
+        return (a + b);
+}
+int sub(int a, int b)
+{
+        return (a - b);
+}
+int mul(int a, int b)
+{
+        return (a * b);
+}
+int div(int a, int b)
+{
+       return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
